3a8e8fce2dee8012c4cec4e217bf6cdbbe30f171
XiaomuWang <15537110473@163.com>
2021-10-20 16:11:01 +0000

    first commit

---
+++ build/CMakeFiles/DDSHelloWorldExampleTCP.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/temp1/HelloWorldExampleTCP/HelloWorld.cxx" "/workspace/temp1/HelloWorldExampleTCP/build/CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorld.cxx.o"
+  "/workspace/temp1/HelloWorldExampleTCP/HelloWorldPubSubTypes.cxx" "/workspace/temp1/HelloWorldExampleTCP/build/CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorldPubSubTypes.cxx.o"
+  "/workspace/temp1/HelloWorldExampleTCP/HelloWorldPublisher.cpp" "/workspace/temp1/HelloWorldExampleTCP/build/CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorldPublisher.cpp.o"
+  "/workspace/temp1/HelloWorldExampleTCP/HelloWorldSubscriber.cpp" "/workspace/temp1/HelloWorldExampleTCP/build/CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorldSubscriber.cpp.o"
+  "/workspace/temp1/HelloWorldExampleTCP/HelloWorld_main.cpp" "/workspace/temp1/HelloWorldExampleTCP/build/CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorld_main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FOONATHAN_MEMORY=1"
+  "FOONATHAN_MEMORY_VERSION_MAJOR=0"
+  "FOONATHAN_MEMORY_VERSION_MINOR=6"
+  "FOONATHAN_MEMORY_VERSION_PATCH=2"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/foonathan_memory/comp"
+  "/usr/local/include/foonathan_memory"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/DDSHelloWorldExampleTCP.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorld.cxx.o"
+  "CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorldPubSubTypes.cxx.o"
+  "CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorldPublisher.cpp.o"
+  "CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorldSubscriber.cpp.o"
+  "CMakeFiles/DDSHelloWorldExampleTCP.dir/HelloWorld_main.cpp.o"
+  "DDSHelloWorldExampleTCP.pdb"
+  "DDSHelloWorldExampleTCP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DDSHelloWorldExampleTCP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
